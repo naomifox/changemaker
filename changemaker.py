faa20e8e795453bcf42bcee9bc1f9e4876eb44cc
Naomi Fox <nkfox@lbl.gov>
2014-08-01 15:40:20 -0700

    Finished debugging ChangeMaker change method.

--- changemaker.py
+++ changemaker.py
@@ -20,7 +20,7 @@ class ChangeMaker:
     def fill_table(self, amount):
         '''
         table has one row, and amount columns
-        table[i]: contains the set of coins that could be added 
+        table[i]: contains the set of coin denominations that could be added 
                   to previously computed values in order to 
                   make up the amount, or None if its not possible
                   with the set of denominations
@@ -29,7 +29,6 @@ class ChangeMaker:
         table = [ None for i in range(1, amount + 1) ] 
         for i in range(1, amount + 1):
             for coin_val in self.coin_denoms:
-                print "coin_val", coin_val, "i", i, "table", table
                 if coin_val > i:
                     continue
                 if coin_val == i or table[i - coin_val - 1] != None:
@@ -45,6 +44,12 @@ class ChangeMaker:
         for making change.
         
         Just performs DFS to traverse all possible paths.
+
+        Assumption: table was created by fill_table 
+        and it has a solution
+
+        Each combination will be in order, from largest to smallest
+        coin denomination
         '''
         combos = None
         if amount > 0:
@@ -52,7 +57,7 @@ class ChangeMaker:
             for coin_val in table[amount - 1]:
                 short_combos = self.get_combinations(table, amount - coin_val)
                 if not short_combos:
-                    all_combinations += [[coin_val]]
+                    combos += [[coin_val]]
                 else:
                     combos += [s_c + [coin_val] for s_c in short_combos if s_c[-1] >= coin_val]
         return combos
@@ -63,6 +68,8 @@ class ChangeMaker:
         used
         '''
         table = self.fill_table(amount)
+        if table[-1] == None:
+            return None
         combinations = self.get_combinations(table, amount)
         return combinations
 
@@ -82,7 +89,6 @@ class TestChangeMaker(unittest.TestCase):
         '''
         cm = ChangeMaker([25, 10, 5, 1])
         table = cm.fill_table(1)
-        print table
         self.assertEqual(len(table), 1)
         self.assertEqual(table[0], [1])
 
@@ -93,7 +99,6 @@ class TestChangeMaker(unittest.TestCase):
         '''
         cm = ChangeMaker([25, 10, 5, 1])
         table = cm.fill_table(10)
-        print table
         self.assertEqual(len(table), 10)
         self.assertEqual(len(table[0]), 1)
         self.assertEqual(len(table[9]), 3)
@@ -102,7 +107,6 @@ class TestChangeMaker(unittest.TestCase):
         cm = ChangeMaker([])
         table = [[1], [1], [1], [1], [5, 1], [5, 1], [5, 1], [5, 1], [5, 1], [10, 5, 1]]
         combinations = cm.get_combinations(table, 10)
-        print "combinations", combinations
         self.assertEquals(len(combinations), 4)
 
     def test_change_1(self):
@@ -116,19 +120,50 @@ class TestChangeMaker(unittest.TestCase):
         self.assertEqual(len(combinations), 2)
         self.assertEqual(len(combinations[0]), 4)
         self.assertEqual(len(combinations[1]), 8)
-        
-        
+              
     def test_change_2(self):
         '''
         >>> cm = ChangeMaker([5])
         >>> cm.change(5)
         [[5]]
+        >>> cm.change(1)
+        None
         '''
         cm = ChangeMaker([5])
         combinations = cm.change(5)
         self.assertEqual(len(combinations), 1)
         combinations = cm.change(1)
-        self.assertEqual(len(combinations), 0)
+        self.assertEqual(combinations, None)
+
+    def test_change_3(self):
+        '''
+        Example where greedy algorithm does not suffice
+        >>> cm = ChangeMaker([3, 4, 5, 7])
+        >>> cm.change(10)
+        [[4, 3, 3], [7, 3], [5, 5]]
+        >>> cm.change(1)
+        None
+        '''
+        cm = ChangeMaker([3, 4, 5, 7])
+        combinations = cm.change(10)
+        self.assertEqual(len(combinations), 3)
+        answer = [[4, 3, 3], [7, 3], [5, 5]]
+        for combo in answer:
+            self.assertTrue(combo in combinations)
+        
+        combinations = cm.change(7)
+        self.assertEqual(len(combinations), 2)
+        answer = [[4,3], [7]]
+        for combo in answer:
+            self.assertTrue(combo in combinations)
+
+        cm = ChangeMaker([7, 5, 3, 4])
+        combinations = cm.change(7)
+        self.assertEqual(len(combinations), 2)
+        answer = [[4,3], [7]]
+        for combo in answer:
+            self.assertTrue(combo in combinations)
+
 
 
 
